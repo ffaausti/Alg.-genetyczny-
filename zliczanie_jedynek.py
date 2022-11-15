646564cf665f62f4de835a49e9f83d0787ecf99c
ffaausti <83342735+ffaausti@users.noreply.github.com>
2022-11-15 12:15:08 +0100

    Create zliczanie_jedynek.py
    Zliczanie błędu w wartościach kolumn i wierszy nonogramu i osobnika

---
+++ zliczanie_jedynek.py
@@ -0,0 +1,30 @@
+def zliczanie_wartosci_nonogramu(kolumny, wiersze):
+    wartosci_kolumn_nonogramu = []
+    wartosci_wierszy_nonogramu = []
+
+    for i in range(len(kolumny)):
+        wartosci_kolumn_nonogramu.append(sum(kolumny[i]))
+    for i in range(len(wiersze)):
+        wartosci_wierszy_nonogramu.append(sum(wiersze[i]))
+    return wartosci_wierszy_nonogramu, wartosci_kolumn_nonogramu
+
+def zliczanie_wartosci_osobnika(osobnik_matryca):
+    wartosci_wierszy_osobnika = []
+    wartosci_kolumn_osobnika = []
+    for i in range(5):
+        count = np.count_nonzero(osobnik_matryca[0:,i] == 1)
+        wartosci_kolumn_osobnika.append(count)
+    for i in range(5):
+        count = np.count_nonzero(osobnik_matryca[i,0:] == 1)
+        wartosci_wierszy_osobnika.append(count)
+    return wartosci_wierszy_osobnika, wartosci_kolumn_osobnika
+
+def porownywanie_wartosci_1(wartosci_kolumn_nonogramu,wartosci_wierszy_nonogramu, wartosci_kolumn_osobnika, wartosci_wierszy_osobnika):
+    lista_bledow = []
+    for i in range(len(kolumny)):
+        blad = abs(wartosci_kolumn_nonogramu[i]-wartosci_kolumn_osobnika[i])
+        blad2 = abs(wartosci_wierszy_nonogramu[i]-wartosci_wierszy_osobnika[i])
+        lista_bledow.append(blad)
+        lista_bledow.append(blad2)
+        
+    return sum(lista_bledow)

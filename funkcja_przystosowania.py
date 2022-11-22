8bbd66fd7d11778e7ebf8a3ee8d60275eb115a49
ffaausti <83342735+ffaausti@users.noreply.github.com>
2022-11-22 08:44:46 +0100

    Create funkcja_przystosowania.py

---
+++ funkcja_przystosowania.py
@@ -0,0 +1,4 @@
+def funkcja_przystosowania(lista_bledow, lista_bledow_0):
+    return lista_bledow + lista_bledow_0
+    
+lista_bledow = porownywanie_wartosci_1(wartosci_kolumn_nonogramu, wartosci_wierszy_nonogramu, wartosci_kolumn_osobnika, wartosci_wierszy_osobnika)
